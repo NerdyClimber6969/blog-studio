67cb945398ce5ca62101c42bea342a33b4a84f4c
Nick Yuen <138299516+NerdyClimber6969@users.noreply.github.com>
2025-06-22 18:34:34 +0800

    Add StatCardSkeleton component to DashboardPage for improved loading state

--- src/components/StatCard/StatCardSkeleton.jsx
+++ src/components/StatCard/StatCardSkeleton.jsx
@@ -1,20 +1,20 @@
 import cardStyles from '../Card/Card.module.css';
 import { SkeletonBox, SkeletonText } from '../Skeleton';
 
-function RecordCardSkeleton({ className }) {
+function StatCardSkeleton({ className }) {
     return (
         <div className={`${cardStyles.card} ${className}`}>
-            <SkeletonBox height={25} className='mb2'/>
-            <SkeletonText 
-                lines={2} 
+            <SkeletonText
+                lines={2}
                 width={200} 
-                height={15} 
+                height={20} 
                 gap={10} 
                 className='mb4'
             />
-            <SkeletonBox height={20} width={300} className='mb2'/>
+            <SkeletonBox height={15}  className='mb2'/>
         </div>
     );
 };
 
-export default RecordCardSkeleton;
+
+export default StatCardSkeleton;
